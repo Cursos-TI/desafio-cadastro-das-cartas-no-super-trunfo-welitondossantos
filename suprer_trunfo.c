30de9ed42af7a39c84aace200bb97b31fd33b476
welitondossantos <welitonalbuquerque31@gmail.com>
2025-04-20 22:33:28 +0000

    variavel2

--- suprer_trunfo.c
+++ suprer_trunfo.c
@@ -3,11 +3,18 @@
 int main(){
     printf("Desafio super trunfo!\n");
 
-    int NomedaCidade;
+    int NomedaCidade1;
     char Estado, codigo;
     float Area[20];
     float Pib[20];
     int PontosTuristicos;
+    
+    int NomedaCidada2;
+    char Estado2, codigo2;
+    float Area2[20];
+    float Pib2[20];
+    int PontosTuristicos2;
+    
     
     
 
