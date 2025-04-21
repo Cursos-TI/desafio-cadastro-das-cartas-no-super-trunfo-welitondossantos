2d02090e571073d19ba418be09580cbcd08be502
welitondossantos <welitonalbuquerque31@gmail.com>
2025-04-21 00:40:07 +0000

    epecificando as variavel

--- suprer_trunfo.c
+++ suprer_trunfo.c
@@ -4,17 +4,33 @@ int main(){
     printf("Desafio super trunfo!\n");
 
     int NomedaCidade1;
-    char Estado, codigo;
-    float Area[20];
-    float Pib[20];
-    int PontosTuristicos;
+    char Estado1[4];
+    char codigo1[4];
+    float Area1;
+    float Pib1;
+    int PontosTuristicos1;
     
     int NomedaCidada2;
-    char Estado2, codigo2;
-    float Area2[20];
-    float Pib2[20];
+    char Estado2[4];
+    char codigo2[4];
+    float Area2;
+    float Pib2;
     int PontosTuristicos2;
+
+    printf("Entre com o  NomedaCidade1 e NomedaCidade2: \n");
+    scanf("%d, &NomedaCidade1, NomedaCidade2: \n");
+
+    printf("Entre com o Estado1, codigo1, Estado2, codigo2: \n");
+    scanf("%s, &Estado, codigo1 \n, Estado, codigo2: \n");
+
+    printf("Entre com a Area1, Area2: \n");
+    scanf("%f, &Area1, Area2: \n");
+   
+
     
+
+
+
     
     
 
